44c7c63dbc6cf83d2b272d2428ba4e7cb03f51ba
tim <Henning-tim@gmx.de>
2024-05-30 19:43:35 +0200

    Changed layout

--- map.py
+++ map.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('symbol_map.html')
+
+@app.route('/map')  # Creating a new route for the choro_map.html
+def map():
+    return render_template('choro_map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
